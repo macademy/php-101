a545a24eb5509b2db810e9a0f26ee648edaf18b7
Mark Shust <mark@shust.com>
2022-03-09 11:14:38 -0500

    Create a constant in PHP to store a fixed value

--- index.php
+++ index.php
@@ -1,7 +1,12 @@
 <?php
 $title = 'My Blog';
 $numPosts = 10;
+$numPosts = $numPosts + 10;
+define('MIN_NUM_POSTS', 0);
+const MAX_NUM_POSTS = 100;
 $numPostsDisplay = "\"$numPosts\" posts";
 ?>
 <h1><?= $title ?></h1>
 <h2><?= $numPostsDisplay ?></h2>
+<h3>Minimum: <?= MIN_NUM_POSTS ?></h3>
+<h3>Maximum: <?= MAX_NUM_POSTS ?></h3>
