605a0a48a5e80c3565490f9ccfe49490aa526ef2
Mark Shust <mark@shust.com>
2022-03-14 14:55:51 -0400

    Conditionally execute PHP code with an if statement

--- index.php
+++ index.php
@@ -6,4 +6,8 @@ $numPostsDisplay = "\"$numPosts\" posts";
 ?>
 <h1><?= $title ?></h1>
 <h2><?= $numPostsDisplay ?></h2>
-<?php var_dump($hasPosts) ?>
+<?php
+if ($hasPosts) {
+    echo 'Posts exist.';
+}
+?>
