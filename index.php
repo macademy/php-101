58574962ca1fb18f53f848fed46e02af5bd59c9b
Mark Shust <mark@shust.com>
2022-03-15 10:13:08 -0400

    Write multiple conditions to execute alternate PHP code

--- index.php
+++ index.php
@@ -7,7 +7,11 @@ $numPostsDisplay = "\"$numPosts\" posts";
 <h1><?= $title ?></h1>
 <h2><?= $numPostsDisplay ?></h2>
 <?php
-if ($hasPosts) {
+if ($numPosts === 3) {
+    echo 'There are exactly 3 posts.';
+} elseif ($hasPosts) {
     echo 'Posts exist.';
+} else {
+    echo 'There are no posts.';
 }
 ?>
