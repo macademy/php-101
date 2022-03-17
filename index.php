f918fbedbf65754e87057d7f4215cb73c88393ca
Mark Shust <mark@shust.com>
2022-03-17 11:39:38 -0400

    Refactor a PHP if else statement into a ternary

--- index.php
+++ index.php
@@ -6,12 +6,4 @@ $numPostsDisplay = "\"$numPosts\" posts";
 ?>
 <h1><?= $title ?></h1>
 <h2><?= $numPostsDisplay ?></h2>
-<?php if ($hasPosts): ?>
-    <?php if ($numPosts === 3): ?>
-        There are exactly 3 posts.
-    <?php else: ?>
-        Posts exist.
-    <?php endif ?>
-<?php else: ?>
-    There are no posts.
-<?php endif ?>
+<?= $hasPosts ? 'Posts exist.' : 'There are no posts.' ?>
