d59369d3a7adb3b11576eb6a1f9e584ecd1098b9
Mark Shust <mark@shust.com>
2022-03-28 11:59:13 -0400

    Create a list using a PHP array

--- index.php
+++ index.php
@@ -3,12 +3,7 @@ $title = 'My Blog';
 $numPosts = 10;
 $hasPosts = $numPosts > 0;
 $numPostsDisplay = "\"$numPosts\" posts";
-$message = match ($numPosts) {
-    0 => 'There are no posts.',
-    1, 2, 3 => 'There are some posts.',
-    default => 'There are many posts.',
-};
+$tags = ['php', 'docker', 'mysql'];
 ?>
 <h1><?= $title ?></h1>
 <h2><?= $numPostsDisplay ?></h2>
-<p><?= $message ?></p>
