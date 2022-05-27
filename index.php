278f31eedb638c15883c346e0fdf3aaa39d09866
Mark Shust <mark@shust.com>
2022-05-27 13:47:39 -0400

    Continue or break out of a PHP loop

--- index.php
+++ index.php
@@ -9,6 +9,10 @@ $posts = [
         'title' => 'How to learn MySQL',
         'content' => 'This is how you learn MySQL.',
     ],
+    [
+        'title' => 'How to learn Nginx',
+        'content' => 'This is how you learn Nginx.',
+    ],
 ];
 $numPosts = count($posts);
 $postText = $numPosts === 1 ? 'post' : 'posts';
@@ -17,6 +21,11 @@ $numPostsDisplay = "$numPosts $postText";
 <h1><?= $title ?></h1>
 <h2><?= $numPostsDisplay ?></h2>
 <?php for ($i = 0; $i < $numPosts; $i++) : ?>
+    <?php
+    if (str_contains($posts[$i]['title'], 'SQL')) :
+        break;
+    endif
+    ?>
     <h3><?= $posts[$i]['title'] ?></h3>
     <p><?= $posts[$i]['content'] ?></p>
 <?php endfor ?>
